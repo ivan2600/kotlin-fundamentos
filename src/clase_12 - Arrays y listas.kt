30dc4380ef5e7ba2f13665879f1609be425135f5
Ivan <ivan.adamo.8@gmail.com>
2025-10-28 01:44:47 +0000

    arrays y ciclos

---
+++ src/clase_12 - Arrays y listas.kt
@@ -0,0 +1,42 @@
+fun main() {
+
+    val emails = arrayOf("ivan@quicklysabor.com", "osvaldo@quicklysabor.com", "ivana@quicklysabor.com")
+    val cantidad = arrayOf(1, 15, 50, 100)
+
+    println("El primer correo es: ${ emails[0] }")
+    //Se pueden reemplazar valores facilmente (las dos formas son equivalentes)
+    //emails[0] = "ivan.adamo.8@gmail.com"
+    emails.set(0, "ivan.adamo.8@gmail.com")
+    emails[2] = "dio.bruno.8@gmail.com"
+
+    println(emails.get(1))
+    println(emails[2])
+    //Como imprimir un array COMPLETO?
+    println("La lista de correos es: ${ emails.joinToString() }.")
+    //Imprimir el tamaño de un array
+    println("El tamaño del array es de ${ emails.size } pocisiones.")
+
+    //Para crear un array modificable...
+    val nuevaListaDeEmails = mutableListOf<String>()
+    println("Nueva lista: ${ nuevaListaDeEmails.joinToString() }, tamaño: ${ nuevaListaDeEmails.size }")
+    nuevaListaDeEmails.addAll(arrayOf("ivan@quicklysabor.com", "osvaldo@quicklysabor.com", "ivana@quicklysabor.com"))
+    println("Nueva lista: ${ nuevaListaDeEmails.joinToString() }, tamaño: ${ nuevaListaDeEmails.size }")
+    nuevaListaDeEmails.remove("ivana@quicklysabor.com")
+    println("Nueva lista: ${ nuevaListaDeEmails.joinToString() }, tamaño: ${ nuevaListaDeEmails.size }")
+    nuevaListaDeEmails.add(1, "dio.bruno.8@gmail.com")
+    println("Nueva lista: ${ nuevaListaDeEmails.joinToString() }, tamaño: ${ nuevaListaDeEmails.size }")
+    println(nuevaListaDeEmails)
+
+    val lista = listOf<String>("Megadeth", "Metallica", "Pantera")
+    println(lista)//las listas se imprimen bien - pero no se puede modificar el contenido.
+    println(emails.joinToString())//los arrays necesitan este metodo de mierda para imprimirse
+    println("=========================================================================")
+    //EJERCICIO
+
+    val listaDeAsuntos = mutableListOf<String>()
+    listaDeAsuntos.addAll(arrayOf("Saludos", "Urgente", "Gratis"))
+    listaDeAsuntos[1] = "Tu vieja"
+    listaDeAsuntos.remove("Saludos")
+    println("Lista de asuntos $listaDeAsuntos, tamaño: ${ listaDeAsuntos.size }")
+
+}
--- src/ejercicio_01 - Calculadora.kt
+++ src/ejercicio_01 - Calculadora.kt
@@ -1,14 +1,14 @@
 fun  calculadora() {
 
     print("Ingrese el primer número: ")
-    val primerNumero = readlnOrNull()?.toIntOrNull() ?: 1
+    val primerNumero = readlnOrNull()?.toDoubleOrNull() ?: 1.0
 
     print("Ingrese un operador: ")
     val operador = readlnOrNull() ?: "+"
 
     print("Ingrese el segundo número: ")
-    val segundoNumero = readlnOrNull()?.toIntOrNull() ?: 1
-    if (operador == "/" && segundoNumero == 0) {
+    val segundoNumero = readlnOrNull()?.toDoubleOrNull() ?: 1.0
+    if (operador == "/" && segundoNumero == 0.0) {
         println("ERROR")
     } else {
         when (operador) {
