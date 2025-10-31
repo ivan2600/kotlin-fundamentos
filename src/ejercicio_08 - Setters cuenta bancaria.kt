79fdb79acefeddcf601766e33e1f7c824b16453e
Ivan <ivan.adamo.8@gmail.com>
2025-10-31 00:10:05 +0000

    getters y setters

---
+++ src/ejercicio_08 - Setters cuenta bancaria.kt
@@ -0,0 +1,38 @@
+class CuentaBancaria {
+    var titular: String = "Sin titular"
+        get() = field.replaceRange(3, field.length, "***")
+        set(value) {
+            if (value.isEmpty()) {
+                println("Este campo no puede estar vacio.")
+            } else {
+                field = value
+                println("El titular de esta cuenta es: $value.")
+            }
+        }
+    var saldo: Double = 0.0
+        get() {
+            println("Consultando saldo...")
+            return field
+        }
+        set(value) {
+            if (value < 0.0) {
+                println("El valor no puede ser negativo.")
+            } else {
+                field = value
+                println("El saldo de la cuenta ahora es de $$value.")
+            }
+        }
+
+}
+
+fun main() {
+
+    val cuenta1 = CuentaBancaria()
+
+    cuenta1.titular = ""
+    cuenta1.titular = "Ivan"
+    cuenta1.saldo = -100.0
+    cuenta1.saldo = 1000.0
+
+    println("El saldo de ${ cuenta1.titular } es de $${ cuenta1.saldo }")
+}
